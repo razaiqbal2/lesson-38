66a900c1e7556e5b997c025bb32adaac0e4a5723
razaiqbal2 <duaf0226@gmail.com>
2024-12-14 17:20:40 +0500

    project

--- activity3.py
+++ activity3.py
@@ -2,11 +2,13 @@ first_name="Syed"
 last_name="Rizvi"
 middle_name="Raza Iqbal"
 in_one_go=first_name,middle_name,last_name
+example="ha"*8
 
 print("First name" , first_name)
 print("Last name" , middle_name)
 print("Middle name" , last_name)
 print("full name" , in_one_go)
+print("Print ha 8 times",example)
 
 word="hydrochloric_acid_(HCL)"
 print("the total amout of words in ",len(word))
