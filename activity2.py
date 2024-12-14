721e729daf5c529991e27059a0faf054727ddb90
razaiqbal2 <duaf0226@gmail.com>
2024-12-14 16:37:24 +0500

    operater

---
+++ activity2.py
@@ -0,0 +1,28 @@
+num1=87
+num2=7
+
+print("Number 1" , num1)
+print("Number 2" , num2)
+
+
+#for numerical operater
+print("Addition ",num1+num2)
+print("Subtraction ",num1-num2)
+print("Product ",num1*num2)
+print("Division ",num1/num2)
+print("floor division ",num1//num2)
+print("Modolus ",num1%num2)
+print("Expotentional ",num1**num2)
+
+#condional operater
+print("Equal to ",num1==num2)
+print("Not equal to ",num1!=num2)
+print("Less than ",num1<num2)
+print("Greater than ",num1>num2)
+print("Less than and equal  ",num1<=num2)
+print("Greater than and equal to ",num1>=num2)
+
+#expression
+
+result=num1+num2-num2/num1**num1
+print("Result of my expression" , result)
