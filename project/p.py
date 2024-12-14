6a34269e39cdc7a7540b5b1d82f2569a4e31a97e
razaiqbal2 <duaf0226@gmail.com>
2024-12-14 17:22:16 +0500

    project

---
+++ project/p.py
@@ -0,0 +1,20 @@
+a=input('Enter the value of a:')
+b=input('Enter the value of b:')
+c=input('Enter the value of c:')
+d=input('Enter the value of d:')
+
+#swapping 
+
+pen=a
+a=c
+c=pen
+pen=b
+b=d
+d=pen
+
+
+
+print("the value of a after swapping",a)
+print("the value of b after swapping",b)
+print("the value of c after swapping",c)
+
