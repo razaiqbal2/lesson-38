71799290225923ca6003b66e05a5fe05c88791d8
razaiqbal2 <duaf0226@gmail.com>
2024-12-14 16:54:39 +0500

    string

---
+++ activity3.py
@@ -0,0 +1,15 @@
+first_name="Syed"
+last_name="Rizvi"
+middle_name="Raza Iqbal"
+in_one_go=first_name,middle_name,last_name
+
+print("First name" , first_name)
+print("Last name" , middle_name)
+print("Middle name" , last_name)
+print("full name" , in_one_go)
+
+word="hydrochloric_acid_(HCL)"
+print("the total amout of words in ",len(word))
+print("the last is the symbol of Hydrochloric acid" ,word[19:22])
+print("first letter is", word[0])
+print("last letter is", word[13])
